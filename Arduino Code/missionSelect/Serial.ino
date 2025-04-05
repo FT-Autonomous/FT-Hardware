45286a0106e6dd6c7773d3468519840a3758d77d
Shane <sodonne8@tcd.ie>
2025-04-05 13:53:05 +0100

    Changed the decode function to use less complicated, bitwise operations

--- Arduino Code/missionSelect/Serial.ino
+++ Arduino Code/missionSelect/Serial.ino
@@ -16,25 +16,9 @@ void decode(int val) {
     we assume that the value fits 4 bit binary
   */
 
-  double remainder;
-  //we use a pretty standard conversion method I found on khan academy
-  // only differencs is that instead of storing the ones and zeors found we assign the booleans accordingly
+  go = val & 0b0001;             // LSB (bit 0)
+  moving = (val >> 1) & 0b0001;  // bit 1
+  done = (val >> 2) & 0b0001;    // bit 2
+  EBS = (val >> 3) & 0b0001;     // MSB (bit 3)
 
-  for (int i = 0; i < 4; i++) {
-
-    remainder = val / 2;          //remainder is half of the value, with a decimal place of 0.5 or 0
-    val = floor(remainder);       //set val as truncated remainder (exmpl: 6.5 becomes 6, 6.0 stays 6)
-    remainder = remainder - val;  //remainder is now either 0.5 or 0. from here we WOULD assign the relevant array position with 0 or 1
-
-    //setting a bool = to a double will return false if the double is 0 and true otherwise
-    if (i == 0) {
-      go = remainder;
-    } else if (i == 1) {
-      moving = remainder;
-    } else if (i == 2) {
-      done = remainder;
-    } else {
-      EBS = remainder;
-    }
-  }
 }  //set serial booleans according to integer recieved over serial
