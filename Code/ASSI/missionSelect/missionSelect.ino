c7d6a061bd58185e11fe2c776bf1a8ac052baca8
J-Carrig <jcarrig@tcd.ie>
2025-11-05 19:39:53 +0000

    Decluttered and Reorganised the Git

---
+++ Code/ASSI/missionSelect/missionSelect.ino
@@ -0,0 +1,116 @@
+
+bool firstBlink = true;
+double prevT, currT;
+
+bool firstBlink2 = true;
+double prevT2, currT2;
+
+//************************************
+
+const int ledPinMax = 12;
+const int ledPinMin = 6;  //pin range of LED array mission select
+
+int mode = ledPinMin;  //test
+int prevMode = mode;
+
+const int cyclePin = 2;
+const int selectPin = 3;
+
+bool selected = false;
+bool interrupted = false;
+
+//************************************
+
+void setup() {
+  Serial.begin(9600);
+
+  ASSI_Setup();
+  //initialise all the pins needed for the ASSI LEDs and buttons 
+  
+  for (int i = ledPinMin; i < ledPinMax; i++) {
+    pinMode(i, OUTPUT);
+  }  //set pinmode for missionSelect LED range
+
+  pinMode(cyclePin, INPUT);
+  pinMode(selectPin, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(cyclePin), cycleButton, RISING);
+  attachInterrupt(digitalPinToInterrupt(selectPin), selectButton, RISING);
+}
+
+void cycleButton() {
+  if (!interrupted && !selected) {  //this was set as while for some reason, I see no reason for this and dont remember it having caused an issue before so now using IF instead
+    mode++;
+    interrupted = true;
+  }
+}  // code that cycles mode when appropriate button pressed
+
+void selectButton() {
+  selected = true;
+}
+
+void loop() {
+
+  if (!selected) {  // if no mode has been selected yet
+    blink(mode);    //blink the LED corresponding to the current mode being conidered
+    //Serial.println(mode);
+  } else {
+    digitalWrite(mode, HIGH);  //display chosen mode
+  }
+
+  if (mode > ledPinMax) {
+    mode = ledPinMin;
+  }  //loop back around if we have cycled out of bounds
+
+  if (prevMode > ledPinMax) {
+    prevMode = ledPinMin;
+    interrupted = false;
+  }
+
+  if (mode != prevMode) {
+    digitalWrite(prevMode, LOW);
+    prevMode = mode;
+    firstBlink = true;
+    delay(250);
+    interrupted = false;
+  }
+
+  if (selected)
+    checkSerial();
+
+  ASSI();
+}
+
+// Cleavon was here
+void blink(int Pin) {
+  currT = (millis() / 1000);  //get time in integer seconds
+
+  if (firstBlink) {           //if we only just started blinking
+    digitalWrite(Pin, HIGH);  //on
+    prevT = currT;            //save current time
+
+    firstBlink = false;                //set false
+  } else if ((currT - prevT) > 0.5) {  //if firstblink is false and the current time is 2 seconds greater than previous time
+    digitalWrite(Pin, LOW);            //set off
+
+    if ((currT - prevT) > 1) {  //delay another 2 seconds before changing states
+      firstBlink = true;
+    }
+  }
+}
+void blink2(int Pin) {
+  currT2 = (millis() / 1000);  //get time in integer seconds
+
+  if (firstBlink2) {          //if we only just started blinking
+    digitalWrite(Pin, HIGH);  //on
+    prevT2 = currT2;          //save current time
+
+    firstBlink2 = false;                 //set false
+  } else if ((currT2 - prevT2) > 0.5) {  //if firstblink is false and the current time is 2 seconds greater than previous time
+    digitalWrite(Pin, LOW);              //set off
+
+    if ((currT2 - prevT2) > 1) {  //delay another 2 seconds before changing states
+      firstBlink2 = true;
+    }
+  }
+}
---
+++ Code/Rigby/Encoder-Code/Encoder_FT/Encoder_FT.ino
@@ -0,0 +1,182 @@
+//#include "Arduino.h"
+
+//ArduPID C1;
+
+#define ENCODER_A 2 // Channel A pin
+#define ENCODER_B 3 // Channel B pin
+
+#define MOTOR_F 10 // For motor stuff
+#define MOTOR_B 11
+
+
+
+
+//double setpoint = 0.1
+//double input;
+//double output;
+
+
+
+volatile int pulseCount = 0; // Number of pulses (Volatile int to load var from RAM and not overload the compiler as the encoder speed and direction change is based on the command driving it)
+unsigned long lastTime = 0; // Time for RPM calculation
+const int pulsesPerRevolution = 600; // Based on the encoder PPR
+
+float v_target = 0.0;
+float v_current = 0.0;
+float error = 0;  //error is dynamic
+float kp = 1; // kp val @ 0.5 works solo for now
+float ki = 0;
+float kd = 0;
+
+int pwm = 0;
+int correction = 0;
+float v_max = 0.8; // the max speed m/s
+float v_input = 0.0;
+
+
+bool hold = false;
+
+
+
+void encoderISR() {
+  // Increment pulse count on every rising edge of Channel A
+  if (digitalRead(ENCODER_A) == HIGH) {
+    if (digitalRead(ENCODER_B) == LOW) {
+      pulseCount++; // GO CLOCKWISE
+    } else {
+      pulseCount--; // GO ANTICLOCKWISE (Could change since the wheel placment would mean the shaft goes ANTICLOCKWISE)
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(1000);
+  
+  // Set up encoder pins
+  pinMode(ENCODER_A, INPUT_PULLUP);
+  pinMode(ENCODER_B, INPUT_PULLUP); //encoder pins
+
+  pinMode(MOTOR_F,OUTPUT);
+  pinMode(MOTOR_B,OUTPUT);  // motor stuff
+
+  //c1.begin(&input, &output, &setpoint, p, i ,d);
+
+ 
+
+
+  
+  // Attach interrupt for encoder
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoderISR, RISING);
+
+  lastTime = millis(); // Initialize time
+}
+
+void loop() {
+
+  if(Serial.available() > 0)
+  {
+    v_input = Serial.parseFloat();
+    if(v_input != 0.0)
+   {  
+     v_target = v_input; // this allows to update the input using the serial mon.
+   }
+  } 
+
+  unsigned long currentTime = millis();
+  
+  // Calculate RPM 
+  if (currentTime - lastTime >= 500) {
+    float rpm = (pulseCount / (float)pulsesPerRevolution) * 60.0; 
+   
+   float Ang_velocity = rpm * M_PI / 60;
+   float linear_velocity = Ang_velocity * 0.3; // LV = anugular velocity * radius (diameter is 6mm so radius is 0.3mm)
+   v_current = round(linear_velocity * 10) / 10.0;
+
+
+    //Serial.print("RPM: ");
+    //Serial.println(rpm);
+
+    //.print(" AV: ");
+    //Serial.println(Ang_velocity);
+
+   //Serial.print("LV: ");
+    //Serial.print(v_current);
+
+    //Serial.println(v_target);
+
+    int pwm_target = ((v_target / v_max) * 255); // feed the PID a value between 0-255, it converts the target velocity from m/s to target pwn val.
+    int pwm_current = ((v_current / v_max) * 255); // PWM 
+    error = pwm_target - pwm_current;
+    correction = (error * kp);
+    pwm = pwm_current + correction;
+    pwm = constrain(pwm, 0, 255);
+
+
+
+
+   Serial.print(pwm_target);
+   Serial.print(" ");
+
+
+   Serial.print(pwm_current);
+   Serial.print(" ");
+
+   Serial.print(correction);
+    Serial.print(" ");
+
+
+    Serial.print(pwm);
+
+
+    Serial.print(" ");
+
+    Serial.print(hold);
+
+    Serial.print(" ");
+
+    Serial.print(v_target);
+    Serial.print(" ");
+
+
+    Serial.println(v_current);
+
+
+    
+
+
+
+
+
+
+    pulseCount = 0; // Reset pulse count
+    lastTime = currentTime; // Update time
+  }
+
+  analogWrite(MOTOR_F, pwm);
+  analogWrite(MOTOR_B, 0); // Drive forward.
+
+
+
+
+
+
+
+
+
+
+
+}
+
+// to do:
+// write a PID 
+// #1: current / max speed * 255
+
+
+// serial write
+
+
+
+
+
+// 0.8 m/s is the max speed @ 8V and 1.6 Amp
