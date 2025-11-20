05cad192cc95e611f833965734c5dc427205065d
AA2125 <aa7249@yahoo.com>
2025-11-20 18:46:00 +0000

    Updated steering

--- Code/Rigby/SteeringCode/new_steering_code/new_steering_code.ino
+++ Code/Rigby/SteeringCode/new_steering_code/new_steering_code.ino
@@ -6,44 +6,74 @@ void setup() {
 
 }
 
-float ang_target = 0.0;
-float ang_current = 0.0;
+float angle_target = 0.0;
+float angle_current = 0.0;
 float error = 0;  //error is dynamic
-float kp = 1;
+float kp = 1; 
 
 int pwm = 0;
 int correction = 0;
-float v_max = 0.8; // the max speed m/s
-float v_input = 0.0;
+float angle_max = 39; // Maxium angle (you can't go any further)
+float angle_input = 0.0;
 
 void loop() {
   // put your main code here, to run repeatedly:
    if(Serial.available() > 0)
   {
-    v_input = Serial.parseFloat();
-    if(v_input != 0.0)
+    angle_input = Serial.parseFloat();
+    if(angle_input != 0.0)
    {  
-     v_target = v_input; // this allows to update the input using the serial mon.
+     angle_target = constrain(angle_input, -angle_max, angle_max); // this allows to update the input using the serial mon.
    }
   }
 
    unsigned long currentTime = millis();
+   unsigned long lastTime = 0;
   
   // Calculate RPM 
   if (currentTime - lastTime >= 500) {
-    float rpm = (pulseCount / (float)pulsesPerRevolution) * 60.0; 
-   
-   float Ang_velocity = rpm * M_PI / 60;
-   float linear_velocity = Ang_velocity * 0.3; // LV = anugular velocity * radius (diameter is 6mm so radius is 0.3mm)
-   v_current = round(linear_velocity * 10) / 10.0;
-
-    
-
+   int value = analogRead(pot);
+   angle_current = map(value, 0, 1023, -angle_max, angle_max); // Map out the angle to (-39 and 39 degrees the max the pot will go)
+   angle_current = round(angle_current * 10) / 10.0;
 }
 
-int pwm_target = ((v_target / v_max) * 255); // feed the PID a value between 0-255, it converts the target velocity from m/s to target pwn val.
-int pwm_current = ((v_current / v_max) * 255); // PWM 
+int pwm_target = ((angle_target / angle_max) * 255); // feed the PID a value between 0-255, it converts the target velocity from m/s to target pwn val.
+int pwm_current = ((angle_current / angle_max) * 255); // PWM 
 error = pwm_target - pwm_current;
 correction = (error * kp);
 pwm = pwm_current + correction;
 pwm = constrain(pwm, 0, 255);
+
+ Serial.print(angle_target);
+   Serial.print("Goal: ");
+
+
+   Serial.print(angle_current);
+   Serial.print("Current: ");
+
+   Serial.print(correction);
+    Serial.print("err: ");
+
+
+    Serial.print(pwm);
+
+
+   //  Serial.print(" ");
+
+   // Serial.print(hold);
+
+   // Serial.print(" ");
+
+    Serial.print(angle_target);
+    Serial.print("New_t ");
+
+
+    Serial.println(angle_current);
+    Serial.print("curr_t ");
+
+
+
+
+    //pulseCount = 0; // Reset pulse count
+    lastTime = currentTime; // Update time
+  }
