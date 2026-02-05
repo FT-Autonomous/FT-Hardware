fedf86bedcee73b9f35e1186d89bd7a5115b9255
mbdalpha <166437418+mbdalpha@users.noreply.github.com>
2026-02-05 23:13:27 +0000

    Multi-character serial is real

--- Code/Networking/FT-Serial/MySketch.ino
+++ Code/Networking/FT-Serial/MySketch.ino
@@ -1,6 +1,9 @@
 
 // Links that helped: https://arduino.github.io/arduino-cli/0.32/getting-started/
 // https://forum.arduino.cc/t/two-ways-communication-between-python3-and-arduino/1219738
+// https://forum.arduino.cc/t/serial-input-basics-updated/382007
+
+
 
 
 void setup() {
@@ -9,11 +12,62 @@ void setup() {
   Serial.println("Bruz");
 }
 
-// loop is very close to how the encoder PID serial is setup
+// loop is very close to how the encoder PID serial is setup 
+// EDIT: not sure if this still applies post redoing serial
 void loop() {
   if (Serial.available() > 0) { 
-    String data = Serial.readStringUntil('\n'); 
+    printSerial();
+  }
+}
+
+// this function mainly exists as proof of concept that we are no longer
+// limited to single char messages to the arduino and to be easily transferable
+String readSerialWithStartEndMarkers(){ 
+  static boolean reading=false;
+  static byte ndx=0;
+  char startMarker='<';
+  char endMarker='>';
+  char readCharacter;
+
+  const byte numChars=32;
+  char receivedCharacters[numChars];
+  boolean newData=false;
+
+
+  while(Serial.available()>0&&!newData){
+    readCharacter=Serial.read();
+
+    if(reading){
+      if(readCharacter!=endMarker){
+        receivedCharacters[ndx++] = readCharacter;
+        if(ndx>=numChars){
+          ndx=numChars-1;
+        }
+      }
+      else{
+        receivedCharacters[ndx]='\0';
+        reading=false;
+        ndx=0;
+        newData=true;
+      }
+    }
+
+    else if(readCharacter==startMarker){
+      reading=true;
+    }
+  }
+
+  if(newData){
+    return receivedCharacters;
+  } else {
+    return "";
+  }
+}
+
+void printSerial(){
+  String serialString = readSerialWithStartEndMarkers();
+  if(serialString!=""){
     Serial.print("received: ");
-    Serial.println(data);
+    Serial.println(serialString);
   }
 }
