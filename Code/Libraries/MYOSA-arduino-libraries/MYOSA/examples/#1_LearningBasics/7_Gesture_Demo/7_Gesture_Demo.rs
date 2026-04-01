85c9c3832d880673f2e5659ce36b32d785a11543
mbdalpha <166437418+mbdalpha@users.noreply.github.com>
2026-04-01 10:11:06 +0100

    7_Gesture_Demo example ported to Rust

---
+++ Code/Libraries/MYOSA-arduino-libraries/MYOSA/examples/#1_LearningBasics/7_Gesture_Demo/7_Gesture_Demo.rs
@@ -0,0 +1,108 @@
+/*
+  This code is developed under the MYOSA (LearnTheEasyWay) initiative of MakeSense EduTech and Pegasus Automation.
+  Code has been derived from internet sources and component datasheets.
+  Existing readily-available libraries would have been used "AS IS" and modified for ease of learning purpose.
+
+  Gesture Demo
+  Connection: Connect the "Light Proximity and Gesture" board from the MYOSA kit with the "Controller" board and power them up.
+  Working: Controller board prints (on Serial Monitor) the gesture detected continuously.
+
+  Synopsis of Light Proximity and Gesture Board
+  MYOSA Platform consists of an Light Proximity and Gesture Board. It is equiped with APDS9960 IC.
+  It is a digital RGB, ambient light, proximity and gesture sensor device with I2C compatible interface.
+  I2C Address of the board = 0x39.
+  Detailed Information about Light Proximity and Gesture board Library and usage is provided in the link below.
+  Detailed Guide: https://drive.google.com/file/d/1On6kzIq3ejcu9aMGr2ZB690NnFrXG2yO/view
+
+  NOTE
+  All information, including URL references, is subject to change without prior notice.
+  Please always use the latest versions of software-release for best performance.
+  Unless required by applicable law or agreed to in writing, this software is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+
+  Modifications
+  1 December, 2021 by Pegasus Automation
+  (as a part of MYOSA Initiative)
+
+  Contact Team MakeSense EduTech for any kind of feedback/issues pertaining to performance or any update request.
+  Email: dev.myosa@gmail.com
+*/
+
+#![no_std]
+#![no_main]
+
+/// Hardware abstraction stubs for Serial, Wire (I2C), and Light/Proximity/Gesture sensor.
+mod hal {
+    pub fn serial_begin(_baud: u32) {}
+    pub fn serial_println(_msg: &str) {}
+    pub fn wire_begin() {}
+    pub fn wire_set_clock(_freq: u32) {}
+    pub fn delay(_ms: u32) {}
+
+    pub const DISABLE: bool = false;
+
+    pub struct LightProximityAndGesture;
+
+    impl LightProximityAndGesture {
+        pub fn new() -> Self { LightProximityAndGesture }
+        pub fn begin(&self) -> bool { true }
+        pub fn ping(&self) -> bool { true }
+        pub fn enable_gesture_sensor(&self, _interrupts: bool) -> bool { true }
+        pub fn get_gesture(&self) -> &str { "" }
+    }
+}
+
+use hal::*;
+
+/* Creating Object of LightProximityAndGesture Class */
+fn create_sensor() -> LightProximityAndGesture {
+    LightProximityAndGesture::new()
+}
+
+/* Setup Function */
+fn setup(lpg: &LightProximityAndGesture) {
+
+    /* Setting up communication */
+    serial_begin(115200);
+    wire_begin();
+    wire_set_clock(100000);
+
+    /* Setting up the LightProximityAndGesture Board. */
+    loop {
+        if lpg.begin() {
+            serial_println("Proximity, Ambient Light, RGB & Gesture sensor is connected...");
+            break;
+        }
+        serial_println("Proximity, Ambient Light, RGB & Gesture sensor is disconnected...");
+        delay(500);
+    }
+    serial_println("APDS9960 initialization completed");
+
+    /* Start running the gesture sensor engine */
+    if lpg.enable_gesture_sensor(DISABLE) {
+        serial_println("Gesture sensor is now running");
+    } else {
+        serial_println("Something went wrong during gesture sensor init!");
+    }
+
+    /* Wait for initialization and calibration to finish */
+    delay(500);
+}
+
+/* Loop Function */
+fn main_loop(lpg: &LightProximityAndGesture) {
+
+    /* Loop function continuously reads gesture data and print */
+    if lpg.ping() {
+        lpg.get_gesture();
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    let lpg = create_sensor();
+    setup(&lpg);
+    loop {
+        main_loop(&lpg);
+    }
+}
